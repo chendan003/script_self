00ac06d00aeb5587b71cc9ac52b18f9559c343fd
chendan <35051130+chendan003@users.noreply.github.com>
2018-07-04 17:54:18 +0800

    Update draw_rec.py

--- face-detect-feature/draw_rec.py
+++ face-detect-feature/draw_rec.py
@@ -1,7 +1,11 @@
 # -*- coding:utf-8 -*-
 
 import cv2
-
+# given rec
 img = cv2.imread('image_path')
 cv2.rectangle(org_img0,  (x1 , y1),(x2,y2),(0,255,0), 1)
 cv2.imwrite('save_image_name', img)
+
+
+new_img[y1:y2, x1:x2, :] = img[y1:y2, x1:x2, :]
+cv2.imwrite(save_path, new_img)
