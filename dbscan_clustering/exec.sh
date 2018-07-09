bbd345218792dd401c3769c5d74154a2a0fa048c
danny chen <dannychen@dannydeMacBook-Pro.local>
2018-07-09 11:05:25 +0800

    updata the api add clustering

---
+++ dbscan_clustering/exec.sh
@@ -0,0 +1,11 @@
+python cluster_test.py  --method DBSCAN \
+    --videoDir /workspace/data/blued_code/face-feature-api/ava-version-python-little-endian/facex_api_response_blue_video_ty_#180611-090055 \
+    --featureList  /workspace/data/blued_code/face-feature-api/ava-version-python-little-endian/ty_npy_list.txt \
+    --picDir /workspace/data/blued_data/ty_camera_2/face_img_crop  \
+    --saveResult True \
+    --saveDir   /workspace/data/blued_data/ty_camera_2/clustering_result_ty/clustering_result_ty_ \
+    --eps 0.45 \
+    --nProcess 16
+
+
+
